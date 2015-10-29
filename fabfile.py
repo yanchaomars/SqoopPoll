35de0ae7b23d4e62f4d3e6eb0c975cc38e209c1d
blemall <blemall@master.(none)>
2015-10-29 10:27:02 -0700

    add conditions

--- fabfile.py
+++ fabfile.py
@@ -1,11 +1,17 @@
 __author__ = 'blemall'
 from fabric.api import *
 
-env.user='root'
-env.hosts = ['10.201.128.157']
+env.user = 'root'
+env.hosts = ['10.201.48.1']
 code_dir = '/root/wuzhen/'
-dest = code_dir + 'Sqoop'
+dest = code_dir + 'SqoopPoll'
 
 
 def deploy():
-    put('/home/blemall/SqoopPoll', dest)
+    # with cd(code_dir):
+    #     sudo('rm -r *')
+    #     sudo('mkdir SqoopPoll')
+    put('/home/blemall/SqoopPoll/', code_dir)
+    # with cd(dest):
+    #     cd('com.bigdata.bailian/')
+    #     run('python concurrent_poll.py')
